8e12a2247338b81e47da80c333bb9b14697e5371
JacobBower <bower.jacob@gmail.com>
2015-04-21 16:55:21 -0700

    added changes on 4-21
    fixed news, and added form

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -4,7 +4,7 @@ class News_model extends CI_Model {
         public function __construct()
         {
                 $this->load->database();
-        }
+        }#end constructor
 		
 		public function get_news($slug = FALSE)
 		{
@@ -16,5 +16,21 @@ class News_model extends CI_Model {
 		
 				$query = $this->db->get_where('news', array('slug' => $slug));
 				return $query->row_array();
+		}#end get-news()
+		
+		public function set_news()
+		{
+			$this->load->helper('url');
+		
+			$slug = url_title($this->input->post('title'), 'dash', TRUE);
+		
+			$data = array(
+				'title' => $this->input->post('title'),
+				'slug' => $slug,
+				'text' => $this->input->post('text')
+			);
+		
+			return $this->db->insert('news', $data);
 		}
+
 }
